9e7296de4b103fd73c6cac138c818c16e5ef0283
Jeremy Warren <jeremyawarren15@gmail.com>
2021-07-21 11:20:36 -0400

    added login form

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
-const LoginPage = () => <h1>Test</h1>;
+const LoginPage = () => (
+  <>
+    <h2>Login</h2>
+    <hr />
+    <form className="col-md-3">
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+        <input type="password" className="form-control" id="exampleInputPassword1" />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  </>
+);
 
 export default LoginPage;
