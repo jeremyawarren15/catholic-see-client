bdf85616cdfb7f560ec2edc34a9b9149b6e81f51
Jeremy Warren <jeremyawarren15@gmail.com>
2021-07-21 00:35:31 -0400

    fixed express hosting site from all routes

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
+app.use(express.static(__dirname));
+app.get(['/', '*'], (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.listen(port);
