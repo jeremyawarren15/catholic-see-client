03e1eca6d4582473292170bc66b57fbc6d9e9bb3
Jeremy Warren <jeremyawarren15@gmail.com>
2021-08-23 00:30:52 -0400

    modified sub request card to be a little better

--- src/components/SubRequestCard.tsx
+++ src/components/SubRequestCard.tsx
@@ -1,14 +1,15 @@
 import {
-  Card, CardActions, CardHeader, IconButton, Tooltip,
+  Box,
+  Card, CardContent, IconButton, Tooltip, Typography,
 } from '@material-ui/core';
 import { AddBox } from '@material-ui/icons';
 import React from 'react';
 
 type Props = {
-  subRequestId:number,
-  date:Date,
-  location:string,
-  handleClaimSubRequest: (subRequestId:number) => void
+  subRequestId: number,
+  date: Date,
+  location: string,
+  handleClaimSubRequest: (subRequestId: number) => void
 }
 
 const SubRequestCard = ({
@@ -16,29 +17,36 @@ const SubRequestCard = ({
   date,
   location,
   handleClaimSubRequest,
-}:Props) => {
+}: Props) => {
   const printDate = () => {
     console.log('something');
     return date.toDateString();
   };
 
   return (
-    <Card elevation={4}>
-      <CardHeader
-        title={printDate()}
-        subheader={location}
-      />
-      <CardActions disableSpacing>
-        <Tooltip title="Claim Sub Request">
-          <IconButton
-            color="success"
-            onClick={() => handleClaimSubRequest(subRequestId)}
-          >
-            <AddBox />
-          </IconButton>
-        </Tooltip>
-      </CardActions>
-    </Card>
+    <Card sx={{
+      display: 'flex',
+      justifyContent: 'space-between'
+    }} elevation={4} >
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <CardContent>
+          <Typography component="div" variant="h5">
+            Sunday 3/3/2021
+          </Typography>
+          <Typography component="div" variant="h5">
+            4-5PM
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            Sacred Heart Chapel
+          </Typography>
+        </CardContent>
+      </Box >
+      <Tooltip title="Claim Sub Request">
+        < IconButton sx={{ alignSelf: 'center', marginLeft: 'auto', marginRight: 'auto' }} color="success">
+          <AddBox sx={{ height: '3em', width: '3em' }} />
+        </IconButton>
+      </Tooltip>
+    </Card >
   );
 };
 
