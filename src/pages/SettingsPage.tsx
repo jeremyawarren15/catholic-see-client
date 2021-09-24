0c54442a0093a6498ba22a79448422daa436442a
Jeremy Warren <jeremyawarren15@gmail.com>
2021-09-24 03:42:50 -0400

    Added fields to settings page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,11 +1,99 @@
-import { Typography } from '@material-ui/core'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import {
+  Button,
+  Checkbox,
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Grid,
+  TextField,
+  Typography
+} from '@material-ui/core'
+import { getUserSettings } from '../service/userService';
+import UserContext from '../contexts/UserContext';
 
 const SettingsPage = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [shouldReceiveNewHoursEmails, setShouldReceiveNewHourEmails] = useState(false);
+  const [shouldReceiveSubRequestEmails, setShouldReceiveSubRequestEmails] = useState(false);
+  const { token } = useContext(UserContext);
+
+  const loadSettings = async () => {
+    const { data } = await getUserSettings(token);
+    setFirstName(data.firstName);
+    setLastName(data.lastName);
+    setEmail(data.email);
+    setShouldReceiveNewHourEmails(data.shouldReceiveNewHoursEmails);
+    setShouldReceiveSubRequestEmails(data.shouldReceiveSubRequestEmails);
+  }
+
+  useEffect(() => {
+    loadSettings();
+  }, [])
+
   return (
-    <Typography variant='h2'>
-      Settings
-    </Typography>
+    <Container component="main" maxWidth="lg">
+      <Typography variant='h4' sx={{ marginBottom: '20px' }}>
+        Settings
+      </Typography>
+      <form>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              id="firstName"
+              label="First Name"
+              variant="outlined"
+              value={firstName}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="lastName"
+              label="Last Name"
+              variant="outlined"
+              value={lastName}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="email"
+              label="Email Address"
+              variant="outlined"
+              disabled
+              value={email}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormGroup>
+              <FormLabel component="legend">Receive Emails for...</FormLabel>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={shouldReceiveNewHoursEmails}
+                  />
+                }
+                label="New Hours"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={shouldReceiveSubRequestEmails}
+                  />
+                }
+                label="Sub Requests"
+              />
+            </FormGroup>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Button>Save Changes</Button>
+        </Grid>
+      </form>
+    </Container>
   )
 }
 
